ab89ae976234bb118cee8dd45becf9439c3022cc
ibtisamath <ibtisamat.soumanou1@gmail.com>
2024-02-26 13:07:55 +0100

    login

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -29,6 +29,9 @@ function getusers ()  {
     .then((data) => {
       localStorage.setItem("token", data.token);
     })
+    .catch((error) => {
+      console.error(error);
+    });
 });
 }
 getusers()
